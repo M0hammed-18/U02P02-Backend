40d922294909d43c2423a37b46a950061933252e
M0hammed-18 <Mohammed.Alweheid.elq.js@tuwaiq.edu.sa>
2021-11-21 10:18:44 +0300

    Start

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require("express")
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT
+
+
+
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`server on ${PORT}`);
+});
